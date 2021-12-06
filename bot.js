75c018dc584610b42cb54872eeb8e4b560811de1
RIXIN101 <maksim1237934@gmail.com>
2021-12-06 23:23:04 +0500

    Adding functionality

--- bot.js
+++ bot.js
@@ -1,5 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
 const encodingJapanese = require('encoding-japanese');
 const QRCode = require('qrcode');
-const UTF8 = require('utf8');
+const utf8 = require('utf8');
+const TOKEN = '5006595568:AAHsMePBzqcuC-p4YoWqCSlhe8dJwKv-uYU';
+const bot = new TelegramBot(TOKEN, {polling: true});
 
+bot.onText(/\/start/, msg => {
+    bot.sendMessage(msg.chat.id, 'Приветсвуем вас в нашем боте.\nОн позволяет генерировать QR коды для любого текста, который вы хотите.\nБудь то _ссылки_ или просто _текст_, который вы хотите передать кому-либо в виде QR кода!', {parse_mode: "Markdown"});
+});
+
+bot.onText(/\/help/, msg => {
+    bot.sendMessage(msg.chat.id, 'Нашим ботом очень просто пользоваться. Просто введите _текст_ или _ссылку_, которую хотите видеть после сканирования QR кода и вам в течении нескольких секунд сразу отправится ответ!', {parse_mode: "Markdown"})
+});
+
+bot.on('message',  msg => {
+    if (msg.text != undefined) {
+        const text = msg.text;
+        QRCodeGenerator(text).then(response => {
+            bot.sendPhoto(msg.chat.id, response);
+        });
+    } else {
+        bot.sendMessage(msg.chat.id, 'Тот формат данных, который вы нам отправили не поддерживается, но в скором времени мы это исправим!');
+    }
+});
+
+const QRCodeGenerator = (text) => {
+    return new Promise((resolve, reject) => {
+        QRCode.toFile('./qr_code_data/qrcode.png', utf8.encode(text), (err) => {
+            if (err) reject(err);
+            console.log('Success generate QRCode');
+            resolve('./qr_code_data/qrcode.png');
+        });
+    });
+};
