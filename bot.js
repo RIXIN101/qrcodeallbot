5c291691900f3e2124f9ac3aac56249f1277812e
RIXIN101 <maksim1237934@gmail.com>
2021-12-11 01:24:26 +0500

    Production N1

--- bot.js
+++ bot.js
@@ -1,17 +1,28 @@
 const TelegramBot = require('node-telegram-bot-api');
 const encodingJapanese = require('encoding-japanese');
 const QRCode = require('qrcode');
-const utf8 = require('utf8');
-const TOKEN = '5006595568:AAHgH1G0bBx2_PEqoNTJe7BdfQoLWGjsXT8';
+const TOKEN = '5006595568:AAEnLGjRZSsYuneP5BBhY-YN3rItJfJxTD4';
 const bot = new TelegramBot(TOKEN, {polling: true});
+const fs = require('fs');
 
 bot.onText(/\/start/, msg => {
     bot.sendMessage(msg.chat.id, 'Приветсвуем вас в нашем боте.\nОн позволяет генерировать QR коды для любого текста, который вы хотите.\nБудь то _ссылки_ или просто _текст_, который вы хотите передать кому-либо в виде QR кода!', {parse_mode: "Markdown"});
 });
 
 bot.onText(/\/help/, msg => {
-    bot.sendMessage(msg.chat.id, 'Нашим ботом очень просто пользоваться. Просто введите _текст_ или _ссылку_, которую хотите видеть после сканирования QR кода и вам в течении нескольких секунд сразу отправится ответ!', {parse_mode: "Markdown"})
+    bot.sendMessage(msg.chat.id, 'Нашим ботом очень просто пользоваться. Просто введите _текст_ или _ссылку_, которую хотите видеть после сканирования QR кода и вам в течении нескольких секунд сразу отправится ответ!\n*Бот может работать некорректно на некоторых устройствах.*', {parse_mode: "Markdown"})
 });
+
+setInterval(() => {
+    fs.readdir(`./qr_code_data`, (err, files) => {
+        if (files.length > 50) {
+            console.log(files);
+            for(let i = 0; i <= files.length; i++) {
+                fs.unlink(`./qr_code_data/${files[i]}`, err => {if (err) throw err;});
+            }
+        }
+    })
+}, 30000);
 
 bot.on('message',  msg => {
     if (msg.text != '/start' && msg.text != '/help') {
@@ -19,20 +30,25 @@ bot.on('message',  msg => {
             bot.sendMessage(msg.chat.id, 'Тот формат данных, который вы нам отправили не поддерживается, но в скором времени мы это исправим!');
         } else {
             const text = msg.text;
-            QRCodeGenerator(text).then(response => {
-                bot.sendPhoto(msg.chat.id, response);
-            });
+            const id = msg.chat.id;
+            QRCodeGenerator(text, id)
+                .then(response => {
+                    bot.sendPhoto(id, response);
+                })
+                .catch(err => {
+                    bot.sendMessage(id, `Вы ввели максимально допустимое значение символов`)
+                });
+
         }
     }
-    
 });
 
-const QRCodeGenerator = (text) => {
+const QRCodeGenerator = (text, id) => {
     return new Promise((resolve, reject) => {
-        QRCode.toFile('./qr_code_data/qrcode.png', utf8.encode(text), (err) => {
+        QRCode.toFile(`./qr_code_data/${id}.png`, text, (err) => {
             if (err) reject(err);
             console.log('Success generate QRCode');
-            resolve('./qr_code_data/qrcode.png');
+            resolve(`./qr_code_data/${id}.png`);
         });
     });
 };
